3a5468d2fd8d5934fc133bbf90e43b436157cc4b
minhoo03 <minhoo0333@gmail.com>
2020-11-17 12:53:58 +0900

    jQuery animation

---
+++ js/main.js
@@ -0,0 +1,76 @@
+$(document).ready(() => {
+    $('.gnb > ul > li').on('mouseenter', () => {
+        $('.snb').css({'display':'inline-block'})
+        $('header').stop().animate({
+            'height':'405px'
+        })
+        $('.snb').stop().animate({
+            'opacity': '1'
+        })
+    })
+
+    $('.gnb > ul').on('mouseleave', () => {
+        $('header').stop().animate({
+            'height':'175px'
+        })
+
+        $('.snb').stop().animate({
+            'opacity': '0'
+        }, 300, () => {
+            $('.snb').css({'display':'none'})
+        })
+    })
+
+    // banner
+    const banner = $('.main > .banner')
+
+    banner.find('h2').animate({'opacity':'1','top' : '0'}, 700)
+    banner.find('p').delay(300).animate({'opacity':'1','top' : '0'}, 700)
+    banner.find('.img').delay(300).animate({'opacity':'1','top' : '0'}, 700)
+
+    // sub header
+    const subHeader = $('.main > .sub-header')
+    const subTop = subHeader.find('.top')
+    const subHeaderTop = subHeader.offset().top
+    console.log(subHeaderTop)
+
+    $(window).on('scroll', () => {
+        const scroll = $(window).scrollTop()
+
+        if(scroll > subHeaderTop){
+            subHeader.addClass('fixed')
+        } else{
+            subHeader.removeClass('fixed')
+        }
+    })
+
+    subTop.on('click', () => {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700)
+    })
+
+    // select menu
+    const menu = $('#menu ul > li')
+    menu.on('mouseenter', (event) => {
+        const target = event.currentTarget
+
+        $(target).find('.ko_title').stop().animate({'top':'50px'},400)
+        $(target).find('.en_title').stop().animate({'top':'95px'},400)
+        $(target).find('.desc').stop().animate({'top':'125px','opacity':'1'},500)
+        $(target).find('.icon').stop().animate({'bottom':'35px','opacity':'1'},300)
+    })
+
+    menu.on('mouseleave', (event) => {
+        const target = event.currentTarget
+
+        $(target).find('.ko_title').stop().animate({'top':'100px'},400)
+        $(target).find('.en_title').stop().animate({'top':'145px'},400)
+        $(target).find('.desc').stop().animate({'top':'200px','opacity':'0'},500)
+        $(target).find('.icon').stop().animate({'bottom':'100px','opacity':'0'},300)
+    })
+
+    menu.on('mouseleave', () => {
+
+    })
+})
